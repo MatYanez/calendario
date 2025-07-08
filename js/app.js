69bb3355098d2f970aad2188cc1dac251a17f96b
MatYanez <emeigriegaa@gmail.com>
2025-07-08 14:00:48 -0400

    a

--- js/app.js
+++ js/app.js
@@ -1,20 +1,19 @@
-const firebaseConfig = {
+// Inicializa Firebase
+firebase.initializeApp({
   apiKey: "AIzaSyBkOn-up3m6LVbEoWZh-7yT-sYFtCN_ja0",
   authDomain: "balesch-c4277.firebaseapp.com",
   projectId: "balesch-c4277",
-  storageBucket: "balesch-c4277.appspot.com",
+  storageBucket: "balesch-c4277.firebasestorage.app",
   messagingSenderId: "866172124153",
   appId: "1:866172124153:web:8ecd00f213e4886c0d9b28"
-};
+});
 
-// Inicializa Firebase
-firebase.initializeApp(firebaseConfig);
-
+// Obtén la instancia de Firestore
 const db = firebase.firestore();
 
-// Ejemplo: lee una colección
+// Ejemplo: prueba leer una colección
 db.collection("proyectos").get().then(snapshot => {
   snapshot.forEach(doc => {
-    console.log(doc.id, doc.data());
+    console.log(doc.id, "=>", doc.data());
   });
-});
+}).catch(console.error);
