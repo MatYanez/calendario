4586384a3f93feb0197e852679a859987d009a90
MatYanez <emeigriegaa@gmail.com>
2025-07-25 16:18:09 -0400

    a

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,12 +1,17 @@
 // js/firebase-init.js
 
-document.write('<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js"><\/script>');
-document.write('<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js"><\/script>');
-document.write('<script src="https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js"><\/script>');
+function loadFirebaseScript(src, callback) {
+  const script = document.createElement('script');
+  script.src = src;
+  script.onload = callback;
+  script.defer = true;
+  document.head.appendChild(script);
+}
 
-document.addEventListener('DOMContentLoaded', () => {
-  const interval = setInterval(() => {
-    if (typeof firebase !== 'undefined') {
+// Cargar scripts en orden
+loadFirebaseScript("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js", () => {
+  loadFirebaseScript("https://www.gstatic.com/firebasejs/8.10.1/firebase-auth.js", () => {
+    loadFirebaseScript("https://www.gstatic.com/firebasejs/8.10.1/firebase-firestore.js", () => {
       const firebaseConfig = {
         apiKey: "AIzaSyBkOn-up3m6LVbEoWZh-7yT-sYFtCN_ja0",
         authDomain: "balesch-c4277.firebaseapp.com",
@@ -16,7 +21,9 @@ document.addEventListener('DOMContentLoaded', () => {
         appId: "1:866172124153:web:8ecd00f213e4886c0d9b28"
       };
       firebase.initializeApp(firebaseConfig);
-      clearInterval(interval);
-    }
-  }, 100);
+    });
+  });
 });
+
+
+// v1
