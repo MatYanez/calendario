6e37efb4c2c0ba3aa6f5b6004c875f0559dc373e
MatYanez <emeigriegaa@gmail.com>
2025-07-25 17:26:01 -0400

    a

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,11 +1,12 @@
 // js/firebase-init.js
-var firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_DOMINIO.firebaseapp.com",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_BUCKET.appspot.com",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
+  var firebaseConfig = {
+        apiKey: "AIzaSyBkOn-up3m6LVbEoWZh-7yT-sYFtCN_ja0",
+        authDomain: "balesch-c4277.firebaseapp.com",
+        projectId: "balesch-c4277",
+        storageBucket: "balesch-c4277.appspot.com",
+        messagingSenderId: "866172124153",
+        appId: "1:866172124153:web:8ecd00f213e4886c0d9b28"
+
 };
 
 firebase.initializeApp(firebaseConfig);
